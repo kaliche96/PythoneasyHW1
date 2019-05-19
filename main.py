c5027167bcff299873f68a3ff87060f999dd0c89
Renán Acevedo <41135157+kaliche96@users.noreply.github.com>
2019-05-19 11:03:47 -0600

    HW1

---
+++ main.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 14 09:14:36 2019
+
+@author: JACEVEDO01
+"""
+
+#This file is about types of variables
+
+NameSong = "Wish you were here"
+
+Artist= "Pink Floyd"
+
+DurationInSeconds= 320
+
+YearReleased= "12"+' '+"September" + ' ' + "1975"
+
+Copies= "13.000.000"
+
+Vocals = "David Gilmour"
+
+AlbumRate = 9.5
+
+print (NameSong)
+print (Artist)
+print (YearReleased)
+print (Copies)
+print (Vocals)
+print (AlbumRate)
